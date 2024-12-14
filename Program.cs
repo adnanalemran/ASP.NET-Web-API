3b78c37437ffb1a7c392501745f85ea577bf1628
adnanalemran <adnanalemranontor@gmail.com>
2024-12-14 19:11:16 +0600

    Refactor Program.cs to implement MyClass with array length output

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class MyClass
+{
+
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 5, 34, 3434, 3434, 343, 4, 3434, 34 };
+        Console.WriteLine($"Array Length: {numbers.Length}");
+    }
+
+}
