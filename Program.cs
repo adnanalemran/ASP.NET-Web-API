b5af0d12d0a8d684fa0a352ef56a206ecc03a623
adnanalemran <adnanalemranontor@gmail.com>
2024-12-16 00:55:05 +0600

    Remove unused files and update .gitignore for better project structure; add initial ASP.NET Web API project setup

--- Program.cs
+++ Program.cs
@@ -1,22 +1,41 @@
-﻿public class MyClass
-{
-    public static void Main(string[] args)
-    {
+var builder = WebApplication.CreateBuilder(args);
 
-        List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        var evenNumbers = numbers.Where(n => n % 2 == 0);
-        Console.WriteLine($"count of even numbers: {evenNumbers.Count()}");
-        if (evenNumbers.Count() > 0)
-        {
-            Console.WriteLine("Even numbers are:");
-            foreach (var number in evenNumbers)
-            {
-                Console.WriteLine(number);
-            }
-        }
-        else
-        {
-            Console.WriteLine("No even numbers found");
-        }
-    }
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
