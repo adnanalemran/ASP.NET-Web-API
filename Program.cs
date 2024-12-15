088c64fb8e4c3f7d8345c3826a5e0e9f5dcd0664
adnanalemran <adnanalemranontor@gmail.com>
2024-12-16 01:27:11 +0600

    ASP.NET Web API: Simple Endpoints

--- Program.cs
+++ Program.cs
@@ -4,6 +4,24 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.MapGet("/", () =>
+{
+    Console.WriteLine("Hit it test ");
+    return Results.Json(new
+    {
+        message = "Api working fine ",
+        date = DateTime.Now,
+        name = "Test"
+    });
+
+});
+
+
+app.MapGet("hello", () =>
+{
+    Console.WriteLine("Hit it Hello ");
+    return "Test";
+});
 
 
 app.Run();
