38cdd4c85f4f680999825249dcbda8908061dd83
adnanalemran <adnanalemranontor@gmail.com>
2024-12-18 03:34:56 +0600

    Category CRUD REST ASP.NET Web API

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+List<Category> categories = new List<Category>();
 
 app.MapGet("/", () =>
 {
@@ -17,11 +21,95 @@ app.MapGet("/", () =>
 });
 
 
-app.MapGet("hello", () =>
+//Read => Get: /api/categories 
+app.MapGet("/api/categories", () =>
 {
-    Console.WriteLine("Hit it Hello ");
-    return "Test";
+    return Results.Json(new
+    {
+        status = 200,
+        data = categories,
+
+    });
+
+});
+//Read => Get: /api/categories/{id}
+app.MapGet("/api/categories/{id}", (Guid id) =>
+{
+    var category = categories.FirstOrDefault(x => x.CategoryId == id);
+    if (category == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Json(new
+    {
+        status = 200,
+        data = category,
+
+    });
 });
 
 
+//Create => Post: /api/categories
+app.MapPost("/api/categories", () =>
+{
+    var newCategory = new Category
+    {
+
+        CategoryId = Guid.NewGuid(),
+        Name = "ELeoctonic 2",
+        Description = "Test Dis 2 ",
+        CreatedAt = DateTime.UtcNow,
+
+    };
+    categories.Add(newCategory);
+    return Results.Created($"/api/categories/{newCategory.CategoryId}", newCategory);
+
+});
+
+//Delete => Delete: /api/categories/{id}
+app.MapDelete("/api/categories/{id}", (Guid id) =>
+{
+    var category = categories.FirstOrDefault(x => x.CategoryId == id);
+    if (category == null)
+    {
+        return Results.NotFound();
+    }
+    categories.Remove(category);
+    return Results.NoContent();
+});
+
+//Update => Put: /api/categories/{id}
+app.MapPut("/api/categories/{id}", (Guid id, Category category) =>
+{
+    var existingCategory = categories.FirstOrDefault(x => x.CategoryId == id);
+    if (existingCategory == null)
+    {
+        return Results.NotFound();
+    }
+    existingCategory.Name = category.Name;
+    existingCategory.Description = category.Description;
+
+    return Results.Json(new
+    {
+        status = 200,
+        data = existingCategory
+    });
+});
+
 app.Run();
+
+public record Category
+{
+    public Guid CategoryId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+};
+
+//CRUD
+//Create => Post: /api/categories
+//Read => Get: /api/categories
+//Read => Get: /api/categories/{id}
+//Update => Put: /api/categories/{id}
+//Delete => Delete: /api/categories/{id}
