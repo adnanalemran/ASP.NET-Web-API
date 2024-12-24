7a17e6a8d7d129a16c61a18d27be4c2110748025
Adnan al-emran ontor <68654730+adnanalemran@users.noreply.github.com>
2024-12-24 16:21:40 +0600

    leran oop

---
+++ oop.cs
@@ -0,0 +1,16 @@
+
+using System;
+
+
+class Person
+{
+    public string name;
+    public  int age;
+}
+class Test
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World");
+    }
+}
